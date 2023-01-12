c00423980b9a8bd5777663dd21d31f4c6c494401
masahosono <dev.masahosono@gmail.com>
2023-01-13 01:21:46 +0900

    各種APIのコントローラメソッドの雛形を作成

--- presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/article/UserController.java
+++ presentation/src/main/java/jp/gr/java_conf/simpleblogapi/presentation/article/UserController.java
@@ -3,6 +3,8 @@ package jp.gr.java_conf.simpleblogapi.presentation.article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,36 @@ public class UserController {
 
     @PostMapping(path = "/api/user", produces = "application/json")
     public ResponseEntity<?> registerUser() {
+        return new ResponseEntity<Void>(null);
+    }
+
+    @GetMapping(path = "/api/user/{userId}/balance", produces = "application/json")
+    public ResponseEntity<?> getBalance(
+            @PathVariable("userId") String userId) {
+        return new ResponseEntity<Void>(null);
+    }
+
+    @PostMapping(path = "/api/user/{userId}/balance", produces = "application/json")
+    public ResponseEntity<?> addBalance(
+            @PathVariable("userId") String userId) {
+        return new ResponseEntity<Void>(null);
+    }
+
+    @PostMapping(path = "/api/user/{userId}/payment", produces = "application/json")
+    public ResponseEntity<?> payment(
+            @PathVariable("userId") String userId) {
+        return new ResponseEntity<Void>(null);
+    }
+
+    @PostMapping(path = "/api/user/{userId}/transfer", produces = "application/json")
+    public ResponseEntity<?> transfer(
+            @PathVariable("userId") String userId) {
+        return new ResponseEntity<Void>(null);
+    }
+
+    @PostMapping(path = "/api/user/{userId}/history", produces = "application/json")
+    public ResponseEntity<?> gethistory(
+            @PathVariable("userId") String userId) {
         return new ResponseEntity<Void>(null);
     }
 
