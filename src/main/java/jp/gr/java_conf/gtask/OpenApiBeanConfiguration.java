9d2011972c2746f8d04352d7a18afeb59599082e
masahosono <dev.masahosono@gmail.com>
2023-01-15 15:41:56 +0900

    openapiを導入

---
+++ src/main/java/jp/gr/java_conf/gtask/OpenApiBeanConfiguration.java
@@ -0,0 +1,14 @@
+package jp.gr.java_conf.gtask;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiBeanConfiguration {
+
+    @Bean
+    public OpenAPI restApi() {
+        return new OpenAPI();
+    }
+}
